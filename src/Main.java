5eef524a34f3758d33f6bd60f6084070501dd76f
sergey-kovalev-777 <link2795@yandex.ru>
2026-02-10 21:21:49 +0300

    HW-6

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,75 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("\nHomework 6");
+
+        System.out.println("\nЗадача 1");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nЗадача 2");
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nЗадача 3");
+
+        for (int i = 0; i < 17; i = i+2) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nЗадача 4");
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nЗадача 5");
+
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+        System.out.println("\nЗадача 6");
+
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nЗадача 7");
+
+        for (int i = 1; i <= 512; i = i + i) {
+            System.out.println(i);
+        }
+
+        System.out.println("\nЗадача 8");
+
+        int salary = 29000;
+        int total = 0;
+        for (int i = 0; i <= 12; i++) {
+            total = total + salary;
+        }
+        System.out.println(total);
+
+        System.out.println("\nЗадача 9");
+
+        salary = 29000;
+        total = 0;
+        for (int i = 0; i <= 12; i++) {
+            total = total + total / 100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " Итого " + total);
+        }
+
+        System.out.println("\nЗадача 10");
+
+        int num = 2;
+        int result;
+        for (int i = 1; i <= 10; i++) {
+            result = num * i;
+            System.out.println(num + "*" + i + "=" + result);
+        }
+        }
     }
-}
